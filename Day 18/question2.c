3c4790a5635f898f06587ec7377548a0bd3f6aa7
Keshvi Nema <keshvi311007@gmail.com>
2026-08-27 19:30:00 +0530

    Day 18 - 100 Days of Code

---
+++ Day 18/question2.c
@@ -0,0 +1,26 @@
+// Q36: Write a program to find the HCF (GCD) of two numbers.
+
+#include <stdio.h>
+
+int main()
+{
+    int a, b, i, hcf;
+
+    printf("Enter first number: ");
+    scanf("%d", &a);
+
+    printf("Enter second number: ");
+    scanf("%d", &b);
+
+    for(i = 1; i <= a && i <= b; i++)
+    {
+        if(a % i == 0 && b % i == 0)
+        {
+            hcf = i;
+        }
+    }
+
+    printf("HCF = %d\n", hcf);
+
+    return 0;
+} 
